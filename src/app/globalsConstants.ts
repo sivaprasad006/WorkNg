b20db7089785853ce6765acd3ac2999924666f98
Sivaprasad Mukkamala <sivaprasadmu@VALUEMOMENTUM.BIZ>
2020-01-28 12:31:55 +0530

    gl const

--- src/app/globalsConstants.ts
+++ src/app/globalsConstants.ts
@@ -5,8 +5,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class Appconstants {
   role  = 'test';
   UserInfoDetails = null;
-  apiURL = 'http://qapractice-app:2121/api/';
+  apiURL =  'http://138.91.124.246:1037/api/';
+  // 'http://qapractice-app:2121/api/';
   // 'http://localhost:59381/api/';
+
   LoginFrom = '';
   ProjectDetails: any = [];
 }
